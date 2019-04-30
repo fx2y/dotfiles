652f532e6daff855358a2f685bca8c5012a8d41d
abdullah <44318011+fx2y@users.noreply.github.com>
2019-04-30 11:37:10 +0700

    update

--- workstation-debian/04-provision-docker.sh
+++ workstation-debian/04-provision-docker.sh
@@ -12,7 +12,7 @@ sudo apt-get install -y \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
 echo "2. install docker ce"
 sudo apt-get update
@@ -23,6 +23,7 @@ echo "3. install nvidia-docker"
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
   sudo apt-key add -
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+distribution=debian9
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
   sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update
