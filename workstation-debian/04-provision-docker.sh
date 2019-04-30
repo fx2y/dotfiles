2f4a7e027055218cd96350dba3530ed8954a1d1f
abdullah <44318011+fx2y@users.noreply.github.com>
2019-04-30 11:41:03 +0700

    update

--- workstation-debian/04-provision-docker.sh
+++ workstation-debian/04-provision-docker.sh
@@ -10,9 +10,10 @@ sudo apt-get install -y \
     curl \
     gnupg2 \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable"
 
 echo "2. install docker ce"
 sudo apt-get update
